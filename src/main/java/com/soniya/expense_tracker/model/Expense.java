84ea6209559bdf8aff67f3d17ca3f5833f9509ef
Soniya <sunnapaudel@gmail.com>
2026-01-29 19:48:25 +0545

    Initial commit - Spring Boot expense Tracker

---
+++ src/main/java/com/soniya/expense_tracker/model/Expense.java
@@ -0,0 +1,50 @@
+package com.soniya.expense_tracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double amount;
+    private String category;
+    private String description;
+
+    // getter and setter for fields
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
