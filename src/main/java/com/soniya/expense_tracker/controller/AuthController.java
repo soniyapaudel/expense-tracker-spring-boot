9c45549b120d5ae139efa2716c9032f372afb9b7
Soniya <sunnapaudel@gmail.com>
2026-02-09 19:57:30 +0545

    fix: Implement user registration and login with JWT token

--- src/main/java/com/soniya/expense_tracker/controller/AuthController.java
+++ src/main/java/com/soniya/expense_tracker/controller/AuthController.java
@@ -7,8 +7,6 @@ import com.soniya.expense_tracker.security.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
-import com.soniya.expense_tracker.security.GoogleTokenVerifier;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,15 +15,14 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
-    private final GoogleTokenVerifier googleTokenVerifier;
 
     public AuthController(UserRepository userRepository,
             PasswordEncoder passwordEncoder,
-            JwtUtil jwtUtil, GoogleTokenVerifier googleTokenVerifier) {
+            JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
-        this.googleTokenVerifier = googleTokenVerifier;
+
     }
 
     @PostMapping("/register")
@@ -47,39 +44,6 @@ public class AuthController {
         String token = jwtUtil.generateToken(dbUser.getUsername());
         return Map.of("token", token);
 
-    }
-
-    @PostMapping("/google")
-    public Map<String, String> googleSignIn(@RequestBody Map<String, String> request) {
-        try {
-            String googleToken = request.get("token");
-
-            // verifying the token with Google
-            String email = googleTokenVerifier.getEmailFromToken(googleToken);
-
-            if (email == null) {
-                throw new RuntimeException("Invalid Google token");
-            }
-
-            // check if user laready exists
-            Optional<User> existingUser = userRepository.findByUsername(email);
-            User user;
-
-            if (existingUser.isPresent()) {
-                user = existingUser.get();
-            } else {
-                user = new User();
-                user.setUsername(email);
-                user.setPassword(passwordEncoder.encode("google-" + System.currentTimeMillis()));
-                userRepository.save(user);
-            }
-
-            String jwtToken = jwtUtil.generateToken(user.getUsername());
-            return Map.of("token", jwtToken, "message", "Logged in successfully with Google");
-
-        } catch (Exception e) {
-            throw new RuntimeException("Google authentication failed:" + e.getMessage());
-        }
     }
 
 }
