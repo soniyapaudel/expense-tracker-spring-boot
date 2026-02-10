d241db9aabf2f30cc3842ab8aab0c086b140e241
Soniya <sunnapaudel@gmail.com>
2026-02-10 17:00:50 +0545

    fix: resolve issue in JWT authentication filter

--- src/main/java/com/soniya/expense_tracker/security/JwtAuthFilter.java
+++ src/main/java/com/soniya/expense_tracker/security/JwtAuthFilter.java
@@ -1,16 +1,16 @@
 package com.soniya.expense_tracker.security;
 
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.stereotype.Component;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import org.springframework.stereotype.Component;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -26,34 +26,24 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        try {
-            String jwt = extractJwtFromRequest(request);
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer")) {
+            String token = bearerToken.substring(7);
 
-            if (jwt != null && jwtUtil.validateToken(jwt)) {
-                String username = jwtUtil.extractUsername(jwt);
+            if (jwtUtil.validateToken(token)) {
+                String username = jwtUtil.extractUsername(token);
 
-                // create authentication token
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username,
-                        null, new ArrayList<>());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                // set authentication in security context
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null,
+                        new ArrayList<>());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
 
             }
-        } catch (Exception e) {
-            System.err.println("Cannot set user authentication:" + e.getMessage());
+
         }
 
         filterChain.doFilter(request, response);
-    }
 
-    private String extractJwtFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 
 }
